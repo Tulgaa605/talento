76213696a4f422e041a73d41d21b42a0db509ff2
Tulgaa <munkhtulga0118@gmail.com>
2026-01-26 14:53:04 +0800

    shine

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,24 +1,22 @@
 module.exports = {
-    apps: [
-      {
-        name: "nextapp",
-        script: "node_modules/next/dist/bin/next",
-        args: "start -H 0.0.0.0 -p 80",
-        cwd: process.cwd(),
-        interpreter: "node",
-        watch: false,
-        instances: 1,
-        exec_mode: "fork",
-        env_production: {
-          NODE_ENV: "production",
-          HOSTNAME: "0.0.0.0",
-          PORT: 80
-        },
-        env_development: {
-          NODE_ENV: "development",
-          HOSTNAME: "0.0.0.0"
-        }
+  apps: [
+    {
+      name: "nextapp",
+      script: "node_modules/next/dist/bin/next",
+      args: "start -p 3000",
+      cwd: process.cwd(),
+      interpreter: "node",
+      watch: false,         // PM2 watch-г унтраасан
+      instances: 1,
+      exec_mode: "fork",
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 3000
+      },
+      env_development: {
+        NODE_ENV: "development",
+        PORT: 3000
       }
-    ]
-  };
-  
+    }
+  ]
+};
